08ed9d3246bf9cdfc3f643d768a3aa051872be92
nathan hains <nathanhains14@yahoo.com>
2021-06-09 22:08:32 -0400

    commit tree f0606345ed09577eb4056df2f724a649630c5fdb

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,5 @@
-// Code MovieReviews Here
+import React from 'react'
+const MovieReview = ({reviews}) => <ul className="review-list">
+    {reviews.map(review => <li key={review.display_title} className="review"><h1>{review.display_title}</h1><h2>{review.byline}</h2><h3>{review.summary_short}</h3></li>)}
+</ul>
+export default MovieReview
